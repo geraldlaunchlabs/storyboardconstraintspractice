fa5bb7fdf3a75b7f52011b23f9e0b806919c1a29
Gerald Kim Ico <gerald_ico@launchlabs.ph>
2016-08-03 18:05:35 +0800

    ver 1.0

--- StoryboardConstraintsPractice/StoryboardConstraintsPractice/ViewController.h
+++ StoryboardConstraintsPractice/StoryboardConstraintsPractice/ViewController.h
@@ -8,8 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ViewController : UIViewController{
+    IBOutlet UIImageView *pic1;
+    IBOutlet UIImageView *pic2;
+}
 
 @end
 
